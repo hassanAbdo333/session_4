bcd9bcef89462d4dd0328db452b52c4a0321fb6e
hassankozo333@gmail.com <hassankozo333@gmail.com>
2024-12-21 20:50:04 +0200

    Demo&Assigment

---
+++ assignment4/Program.cs
@@ -0,0 +1,453 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace assignment4;
+
+class Program
+{
+    #region function
+    #region p1
+    public static void swap(int x, int y)//by value
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+
+    }
+    public static void swap2(ref int x,ref int y)//by value
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+
+    }
+    #endregion
+    #region p2
+    public static int sumarr(int[] arr)
+    {
+        int sum = 0;
+        //arr[0] = 100;
+        arr = new int[] { 4, 5, 6 };
+        if (arr != null)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];//sum = sum + arr[i]
+
+            }
+        }
+        return sum;
+
+    }
+    public static int sumarr2(ref int[] arr)
+    {
+        int sum = 0;
+        //arr[0] = 100;
+        arr = new int[] { 4, 5, 6 };
+        if (arr != null)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];//sum = sum + arr[i]
+
+            }
+        }
+        return sum;
+
+    }
+    #endregion
+    #region p3
+    static void sumMul(int x, int y, out int Sum, out int Sub)
+    {
+        Sum = x + y;
+        Sub = x - y;
+    }
+    #endregion
+    #region p4
+    public static int sum(int number)
+    {
+        int x = number / 10;
+        int y = number - (x * 10);
+        int sum = x + y;
+        return sum;
+        
+    }
+    #endregion
+    #region p5
+    public static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+        for (int i = 2;i <Math.Sqrt(number);i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    #endregion
+    #region p6
+    static void MinMaxArray(ref int[] array, out int min, out int max)
+    {
+         max = array[0];
+         min = array[0];
+
+        for (int i = 1; i <array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+
+    }
+    #endregion
+    #region p7
+    public static int factorial(int number)
+    {
+        int factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+
+    }
+    #endregion
+    #region p8
+    public static void ChangeChar(ref string name, char letter, int pos)
+    {
+        char[] charArray = name.ToCharArray();
+
+        charArray[pos] = letter;
+        name = new string(charArray);
+    }
+    #endregion
+    #endregion
+    private static void Main(string[] args)
+    {
+        #region Array
+        #region p01
+        //double[] point1=new double[2];
+        //double[] point2 = new double[2];
+        //double[] point3 = new double[2];
+        //Console.WriteLine("please enter point 1: ");
+        //foreach(int i in point1)
+        //    Console.ReadLine();
+        //double x1=point1[0];
+        //double y1=point1[1];
+        //Console.WriteLine("please enter point 2: ");
+        //foreach (int i in point2)
+        //    Console.ReadLine();
+        //double x2=point2[0];
+        //double y2=point2[1];
+        //Console.WriteLine("please enter point 3: ");
+        //foreach (int i in point3)
+        //    Console.ReadLine();
+        //double x3 =point3[0];
+        //double y3 =point3[1];
+
+        //double area = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+
+        //if (area == 0)
+        //{
+        //    Console.WriteLine("The points lie on a single straight line.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("The points do not lie on a single straight line.");
+        //}
+
+        #endregion
+        #region p02
+        //Console.Write("please Enter the time (in hours) : ");
+        //double timeTaken = double.Parse(Console.ReadLine());
+
+        //if (timeTaken >= 2 && timeTaken < 3)
+        //{
+        //    Console.WriteLine("The worker is highly efficient.");
+        //}
+        //else if (timeTaken >= 3 && timeTaken < 4)
+        //{
+        //    Console.WriteLine("The worker needs to increase their speed.");
+        //}
+        //else if (timeTaken >= 4 && timeTaken <= 5)
+        //{
+        //    Console.WriteLine("The worker is given training to enhance their speed.");
+        //}
+        //else if (timeTaken > 5)
+        //{
+        //    Console.WriteLine("The worker is required to leave the company.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Invalid input. Time should be 2 hours or more.");
+        //}
+        #endregion
+        #region p03
+        //Console.Write("Enter the size of the identity matrix (n): ");
+        //int n = int.Parse(Console.ReadLine());
+        //Console.Write("Enter the Number: ");
+        //int num = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Identity Matrix of size {0} x {0}:", n);
+
+        //for (int i = 0; i < n; i++)
+        //{
+        //    for (int j = 0; j < n; j++)
+        //    {
+        //            Console.Write($" {num}");
+
+        //    }
+        //    Console.WriteLine();
+        //}
+        #endregion
+        #region p04
+
+        //int[] numbers = {1,2,3,4,5}; ;
+        //int Sum = 0;
+        //for (int i = 0; i < numbers.Length; i++)
+        //{
+        //    Sum += numbers[i];
+
+        //}
+        //Console.WriteLine($"The Sum of Array :{Sum}");
+        #endregion
+        #region p05
+        //int[] arr1 = { 1, 2, 3, 4 };
+        //int[] arr2 = { 5, 6, 7 ,8};
+        //int[] arr3 = arr1.Concat(arr2).ToArray();
+        //Array.Sort(arr3);
+        //foreach (int i in arr3)
+        //    Console.WriteLine(i);
+        #endregion
+        #region p06
+
+        //int[] Arr = {2,2,3,3,4};
+        //int[] frequency = new int[5];
+        //int Count = 0;
+        //for (int i = 0; i<5; i++)
+        //{
+        //    for (int j = 0; j <5; j++)
+        //    {
+        //        if (Arr[i] == Arr[j])
+        //        {
+        //            Count++;
+        //        }   
+        //    }
+        //    frequency[i] = Count;
+        //    Count = 0;
+
+        //}
+        //for (int z = 0; z < frequency.Length; z++)
+        //    Console.WriteLine(frequency[z]);
+        #endregion
+        #region p07
+        //Console.Write("Enter the size of the array: ");
+        //int size = int.Parse(Console.ReadLine());
+
+        //int[] array = new int[size];
+        //Console.WriteLine("Enter the elements of the array:");
+        //for (int i = 0; i < size; i++)
+        //{
+        //    Console.Write($"Element {i + 1}: ");
+        //    array[i] = int.Parse(Console.ReadLine());
+        //}
+
+        //int max = array[0];
+        //int min = array[0];
+
+        //for (int i = 1; i < size; i++)
+        //{
+        //    if (array[i] > max)
+        //    {
+        //        max = array[i];
+        //    }
+
+        //    if (array[i] < min)
+        //    {
+        //        min = array[i];
+        //    }
+        //}
+
+        //Console.WriteLine($"Maximum element in the array: {max}");
+        //Console.WriteLine($"Minimum element in the array: {min}");
+        #endregion
+        #region p08
+        //int[] array = {1,7,9,5,3};
+
+        //int max = array[0];
+        //int max02 = array[0];
+
+        //for (int i = 1; i < array.Length; i++)
+        //{
+
+        //    if (array[i] > max)
+        //    {
+        //        max02 = max;
+        //        max = array[i];
+        //    }
+        //    else if (array[i] > max02 && array[i] != max)
+        //    {
+        //        max02 = array[i];
+        //    }
+
+        //}
+
+        //    Console.WriteLine(max02);
+        #endregion
+        #region p09
+        //int[] array = new int[10];
+        //Console.WriteLine("Enter the elements of the array:");
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    array[i] = int.Parse(Console.ReadLine());
+        //}
+        //Dictionary<int, int> firstOccurrence = new Dictionary<int, int>();
+        //int maxDis = -1;
+        //for (int i = 0; i < array.Length; i++)
+        //{
+        //    if (firstOccurrence.ContainsKey(array[i]))
+        //    { 
+        //        int distance = i - firstOccurrence[array[i]] - 1;
+        //        if (distance > maxDis)
+        //        {
+        //            maxDis = distance;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        firstOccurrence[array[i]] = i;
+        //    }
+        //}
+        //Console.WriteLine($"the maxDistance : {maxDis}");
+        #endregion
+        #region p10
+        //Console.WriteLine("Enter a space-separated list of words:");
+        //string input = Console.ReadLine();
+        //string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        //Array.Reverse(words);
+        //string ReverseWords= string.Join(" ", words);
+        //Console.WriteLine(ReverseWords);
+        #endregion
+        #region p11
+        //int[,] a = new int[3,3];
+        //int[,] b = new int[3,3];
+        //for (int i = 0; i < a.GetLength(0); i++)
+        //{
+        //    for (int j = 0; j < a.GetLength(1); j++)
+        //    {
+        //        Console.Write($"Enter value for element ({i + 1}, {j + 1}): ");
+        //        a [i, j] = int.Parse(Console.ReadLine());
+        //    }
+        //}
+
+        //for (int i = 0; i < 3; i++)
+        //{
+        //    for (int j = 0; j < 3; j++)
+        //    {
+        //        b[i, j] = a[i, j];
+        //    }
+        //}
+        //for (int i = 0;i < b.GetLength(0); i++)
+        //{
+        //    for (int j = 0; j < b.GetLength(1); j++)
+        //    {
+        //        Console.Write(b[i,j]+" ");
+        //    }
+        //    Console.WriteLine();
+        //}
+        #endregion
+        #region p12
+        //int[] array = new int[10];
+        //Console.WriteLine("Enter the elements of the array:");
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    array[i] = int.Parse(Console.ReadLine());
+        //}
+        //for (int i = 9;i >= 0; i--)
+        //{
+        //    Console.WriteLine(array[i]);
+        //}
+        #endregion
+        #endregion
+        #region function
+        #region p1
+        //int num1 = 9, num2 = 7;
+        //Console.WriteLine(num1);//9
+        //Console.WriteLine(num2);//7
+        //swap(num1,num2);
+        //Console.WriteLine(num1);//9
+        //Console.WriteLine(num2);//7
+        //swap2(ref num1,ref num2);
+        //Console.WriteLine(num1);//7
+        //Console.WriteLine(num2);//9
+        #endregion
+        #region p2
+        //int[] number = { 1, 2, 3 };
+        //int reslt = sumarr(number);//pasing by value
+        //Console.WriteLine(reslt);
+        //Console.WriteLine(number[0]);
+        ////---------------------------
+        //int[] numbers = { 1, 2, 3 };
+        //int reslts = sumarr2(ref number);// passing by refernce
+        //Console.WriteLine(reslt);
+        //Console.WriteLine(number[0]);
+
+        #endregion
+        #region p3
+        //int A = 10, B = 3, SumResult, SubResult;
+        //sumMul(A, B, out SumResult, out SubResult);// passing by out [output parameter]
+        //Console.WriteLine($"Sum={SumResult}");
+        //Console.WriteLine($"Sub={SubResult}");
+        #endregion
+        #region p4
+        //int num = 25;
+        //int Sum = sum(num);
+        //Console.WriteLine(Sum);
+        #endregion
+        #region p5
+        //int number = 27;
+        //bool result = IsPrime(number);
+
+        //if (result)
+        //{
+        //    Console.WriteLine($"{number} is a prime number.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"{number} is not a prime number.");
+        //}
+        #endregion
+        #region p6
+        //int[] number = { 1, 2, 3 };
+        //int minreslt=0, maxreslt=0;
+        // MinMaxArray(ref number,out minreslt,out maxreslt);
+        //Console.WriteLine($"the Min :{minreslt} , the Max :{maxreslt}");
+        #endregion
+        #region p7
+        //int x = 5;
+        //int f = factorial(x);
+        //Console.WriteLine(f);
+        #endregion
+        #region p8
+        //string Name = "hassan";
+        //char lter = 'Z';
+        //int pos = 5;
+        //Console.WriteLine(Name );
+        //ChangeChar(ref Name,lter,pos);
+        //Console.WriteLine(Name);
+
+
+        #endregion
+        #endregion
+
+    }
+}
---
+++ damoch4/Program.cs
@@ -0,0 +1,468 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace damoch4;
+
+   class Employee
+  {
+    public string firstName { get; set; } = "";
+    public string? lastName { get; set; } = null;
+
+  }
+ class Program
+ {
+    // contain
+    // function
+    // data
+    #region passing by out
+    static void sumMul(int x, int y,out int Sum,out int Mul)
+    {
+         Sum = x + y;
+         Mul = x * y;
+        //return sum;
+        //return Mul;
+        //return new int[] {Sum, Mul }
+
+    
+    }
+    #endregion
+    #region function_params
+    public static int sumarr3(int x, int y=5,params int[] arr)
+    {
+        int sum = 0;
+        if (arr != null)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+
+        }
+
+        return sum;
+    }
+    #endregion
+    #region function
+    public static int sumarr(int[] arr)
+    {
+        int sum = 0;
+        //arr[0] = 100;
+        arr = new int[] { 4, 5, 6 };
+        if (arr != null)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];//sum = sum + arr[i]
+
+            }
+        }
+        return sum;
+
+    }
+    public static int sumarr2(ref int[] arr)
+    {
+        int sum = 0;
+        //arr[0] = 100;
+        arr = new int[] { 4, 5, 6 };
+        if (arr != null)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];//sum = sum + arr[i]
+
+            }
+        }
+        return sum;
+
+    }
+    public static void swap(ref int x, ref int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+
+    }
+    public static void print(string pattern, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(pattern);
+        }
+    }
+
+    public static void printpattern(string pattern = "#", int count = 10)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(pattern);
+        }
+    }
+    #endregion
+    #region Excertion Handling and protective code
+    public static void DoSomeCode() { 
+    //
+    //    try
+    //    {
+    //        int x, y, z;
+    //        Console.WriteLine("please inter x:");
+    //        x = int.Parse(Console.ReadLine());
+    //        Console.WriteLine("please inter y:");
+    //        y = int.Parse(Console.ReadLine());
+    //        z = x / y;
+    //        int[] arr = { 1, 2, 3 };
+    //        arr[100] = 22;
+    //    }
+    //    catch (FormatException ex)
+    //    {
+    //        Console.WriteLine(ex.Message);
+    //    }
+    //    catch (DivideByZeroException ex)
+    //    {
+    //        Console.WriteLine(ex.Message);
+    //    }
+    //    catch (IndexOutOfRangeException ex)
+    //    {
+    //        Console.WriteLine(ex.Message);
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        Console.WriteLine(ex.Message);
+    //    }
+        // Execptions
+        //1-- system Execption
+        //1- formet Execption
+        //2-indexout of range Execption
+        //3-Nullreference Execption
+        //4- invalid opertation Execption
+        //5- arithmetic Execption
+        //5.1- divided by zero Execption
+        //2--Application Execption
+    }
+    public static void DoSomeProtective()
+    {
+        int x, y, z;
+        //bool flag;
+        do
+        {
+            Console.WriteLine("please inter x:");
+            //flag =int.TryParse(Console.ReadLine(), out x);
+        }while (!int.TryParse(Console.ReadLine(), out x));
+        //x = int.Parse(Console.ReadLine());
+        do
+        {
+            Console.WriteLine("please inter y:");
+            //flag =int.TryParse(Console.ReadLine(), out x);
+        } while (!int.TryParse(Console.ReadLine(), out y) || y==0);
+         //y = int.Parse(Console.ReadLine());
+        z = x / y;
+        int[] arr = { 1, 2, 3 };
+        if(arr?.Length>100)
+        {
+            arr[100] = 22;
+        }
+        
+    }
+    #endregion
+    static void Main()
+    {
+        #region string Vs stringBuilder
+        //string name01;
+
+        //name01 = new string("Hassan");
+        //string name02 = "Abdo";
+        //name02 = name01;
+        //name01 = "omar";
+        //Console.WriteLine($"Name 01 is {name01}");
+        //Console.WriteLine($"Name 02 is {name02}");
+        // string is an imutable type cannot change itis value
+        //--------------------------
+        //string message = "Hello";
+        //Console.WriteLine(message);
+        //Console.WriteLine(message.GetHashCode());
+        //message += "hassan";//create new object with different new identity
+        //Console.WriteLine(message);
+        //Console.WriteLine(message.GetHashCode());
+
+        //-------------------------
+        //StringBuilder SB=new StringBuilder();
+        //SB.Append("Hello");
+        //Console.WriteLine(SB);
+        //Console.WriteLine(SB.GetHashCode());
+        //SB.Append("Hassan");
+        //Console.WriteLine(SB);
+        //Console.WriteLine(SB.GetHashCode());
+        ////stringBuilder => Reference type work , work as Reference type
+        //string name ;
+        //name = "Hassan";//12 Bytes
+        //name = "Abdo";//8 Bytes
+
+
+        #endregion
+        #region Array
+        #region one D Array
+        //int[] Number;
+        //Number = new int[3];
+        //Console.WriteLine(Number[0]);
+        //Number[0] = 1;
+        //Number[1] = 2;
+        //Number[2] = 3;
+        //Console.WriteLine(Number[0]);
+        //int[] Number2 = new int[3] { 1, 2, 3 };
+        //Console.WriteLine(Number2[0]);
+        //int[] Number3 = new int[] { 1, 2};
+        //int[] Number4 ={ 1, 2, 3 ,4 ,5 };
+        //Console.WriteLine($"size of Array ={Number4.Length } Number of dimenision ={Number4.Rank}");
+        //for (int i=0; i<Number.Length;i++)
+        //{
+        //    Console.WriteLine(Number[i]);
+        //}
+
+
+
+
+        #endregion
+        #region 2D Array
+        //int[,] mark = new int[3, 5] { { 55, 66, 77, 88, 99 }, { 55, 66, 77, 88, 99}, { 55, 66, 77, 88, 99} };
+        //Console.WriteLine($"size of Array ={mark.Length} ,dimention ={mark.Rank} ");
+        // to get input 
+        //int[,] mark = new int[2, 4];
+        //Console.WriteLine(mark.GetLength(0));//rows
+        //Console.WriteLine(mark.GetLength(1));//colums
+        //for (int i = 0; i < mark.GetLength(0); i++)
+        //{
+        //    Console.WriteLine($"enter mark of students number {i+1}");
+        //    for (int j = 0; j < mark.GetLength(1); j++)
+        //    {
+        //        Console.WriteLine($"sub number {j + 1}");
+        //        mark[i, j] = int.Parse(Console.ReadLine());
+        //    }
+        //}
+        ////to print inputs
+        //for (int i = 0; i < mark.GetLength(0); i++)
+        //{
+        //    Console.WriteLine($"grades of student number {i+1}");
+        //    for (int j = 0; j < mark.GetLength(1); j++)
+        //    {
+        //        Console.WriteLine($"sub number {j + 1}= {mark[i,j]}");
+        //    }
+        //}
+        #endregion
+        #region judged Array
+        // judged Array=> one d array of array with differnet sizes
+        //int[][] number = new int[3][];
+        //number[0]=new int[] {1,2,3};
+        //number[1] = new int[] { 4, 5 };
+        //number[2] = new int[] { 6 };
+        ////print
+        //for (int i = 0; i < number.Length; i++)
+        //{
+        //    for (int j = 0; j < number[i].Length; j++)
+        //    {
+        //        Console.WriteLine(number[i][j]);
+        //    }
+
+        //}
+        #endregion
+
+        #endregion
+        #region Array methods
+        //int[] number = { 5, 1, 5, 3, 4,10 };
+        //int[] arr1 = { 1, 2, 3 };
+        //int[] arr2 = new int[5];
+        #region class member methods [static methods]
+        //Array.Sort(number);// sort array in Asc order
+        //Array.Copy(arr1,arr2,2);
+        //foreach(int num in arr2)
+        //{
+        //    Console.WriteLine(num);//1  2  0  0  0
+        //}
+        //Array.Clear(number);//return value to default value
+
+        // Array.ConstrainedCopy(arr1, 1, arr2, 1, 2);
+        //------------------------------------------
+        //int [] arr3=(int[]) Array.CreateInstance(typeof(int), 10);
+        //int[,,] arr4 = (int[,,])Array.CreateInstance(typeof(int), 2, 4, 3);
+
+        #endregion
+        #region object member methods
+        //arr1.CopyTo(arr2, 1);
+        //foreach (int i in arr2)
+        //{
+        //    Console.WriteLine(i);//{0,1,2,3,0}
+        //}
+        //arr1.SetValue(100, 1);
+        //Console.WriteLine(arr1[1]);
+
+        #endregion
+
+        #endregion
+        #region function 
+        //function has tow components
+        //1- prototype
+        //1- signature
+        //2- return type
+        //3- name
+        //4- paremeters
+        // body
+        //2- calling of function 
+        //print("kkkkk",5);
+        //printpattern();// passing parameters by order
+        // passing parameters by name
+        //printpattern( pattern :"@");// Basing by name
+        #region pasing value type
+        //int a = 3, b = 5;
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
+        //swap(ref a,ref b);
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
+
+
+        #endregion
+        #region passing refernce type
+        //int[] number = {1,2,3};
+        //int reslt = sumarr( number);//pasing by value
+        //Console.WriteLine(reslt);
+        //Console.WriteLine( number[0]);
+        //---------------------------
+        //int[] number = { 1, 2, 3 };
+        //int reslt = sumarr2(ref number);// passing by refernce
+        //Console.WriteLine(reslt);
+        //Console.WriteLine(number[0]);
+
+        #endregion
+        #region passing by out
+        //int A = 10, B = 5, SumResult, MulResult;
+        //sumMul(A, B, out SumResult, out MulResult);// passing by out [output parameter]
+        //Console.WriteLine($"Sum={SumResult}");
+        //Console.WriteLine($"Mul={MulResult}");
+        //int age;
+        //int.TryParse( Console.ReadLine(), out age );
+
+        #endregion
+        #region function_params
+        //int[] Number = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        //int total=sumarr3 (1, 2, 3, 4, 5, 6, 7, 8, 9);
+        //Console.WriteLine(total);
+        #endregion
+
+
+        #endregion
+        #region Boxing Vs unBoxing
+        //Boxing : casting from datatype [value type] to datatype [refernce type]
+        //unBoxing : casting from datatype [refernce type] to datatype [value type]
+        #region example01
+        //object obj;
+        //declare for refernce of type "object",refering to null
+        // this refernce obj can refer to an instance from type object or any another datatype
+        //obj = new object();
+        //obj = new string("hassan");
+        //obj = 'A';//casting from char [value type to object [refernce type]]// implicitly
+        //obj = 3;//casting from int [value type to object [refernce type]]// implicitly//boxing
+        //obj = true;////casting from bool [value type to object [refernce type]]// implicitly//boxing
+        //bool flag = (bool)obj;
+        //Console.WriteLine(flag);//unboxing / explicit casting // unsafe casting
+        //#endregion
+        //#region example02
+        //int x = 5;
+        //object 01 = x;
+
+        #endregion
+        #region example02
+        // boxing
+        //int x = 5;
+        //object a = x;
+        //casting from int [value type to object [refernce type]]// implicitly//boxing
+        // 5  ---------> new object (5)//boxing
+        // parent=child xxxx
+        // animal = dog xxxx
+        // boxing [safe casting]// implicit casting
+        //Console.WriteLine(a);
+        //unboxing
+        //object b = 10;
+        //b = "hassan";
+        //int y = (int)b;
+        //casting from datatype [refernce type] to datatype [value type]//explicit
+        //unboxing unsafe casting :my be there exceptions[invalid casting explicit]
+        // new object (10)-----------> 10
+        // chiled = parent
+
+        #endregion
+        #endregion
+        #region Nullable value type
+        // Null=> is valid value for datetype refernce type only
+        //---------------------------------
+
+        //int? age;// Nullable integer : allow null for int (int value + null)//Nullable<int>age; this code is syntax sugar for int? age;
+        //age = 10;
+        //age = null;
+        //-----------------------------------
+        //nullable double : (double values + null)
+        //Nullable<double>salary;
+        //double? salary;//syntax sugar
+        //salary = 22.5;
+        //salary = null;
+
+
+        #endregion
+        #region how to work with nullable value type
+        //int x = 5;
+        //int? y ;
+        //y = x;// safe
+        ////-----------------
+        //int? k = 10;
+        //k = null;
+        //int l = 20;
+
+        //l =(int) k;// unsafe
+        //----------
+
+        //if (k != null)
+        //{
+        //    l = (int)k;
+        //}
+        //else
+        //{
+        //    l =0;       
+        //}
+        //l=k.HasValue ? k.Value : l;
+        //c oleasing operater
+        //l = k ?? 0;// syntax sugar
+
+        #endregion
+        #region Nullable Refernce type
+        //string message = "hhi";
+        //message = null;
+        //string? message02= null;
+        //Console.WriteLine(message);
+        //Console.WriteLine(message02);
+        //-------------------
+        //string nonull = "hello";
+        //string? Nullable = default;
+        //nonull = Nullable!;//null forgivness 
+
+        #endregion
+        #region Null propagation operater
+        //double x = default;
+        //int[]? number = {1,2,3,4};
+        //for (int i = 0;(number!=null)&&(i<number.Length);i++)//dereferenceing
+        //{
+        //    Console.WriteLine(number[i]);
+        //}
+        //if (number != null)
+        //{
+        //    for (int i = 0;i < number?.Length; i++)//dereferenceing
+        //    {
+        //        Console.WriteLine(number[i]);
+        //    }
+
+        //}
+        //====================
+        //int? length = number?.Length;
+        //number != null ? number.Length : null;//Null propagation operater
+        //int length = number?.Length ?? 0;//coleasing operater
+        #endregion
+        #region Excertion Handling and protective code
+        DoSomeProtective();
+        #endregion
+
+    }
+}
